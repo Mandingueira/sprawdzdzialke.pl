2fdd2fd8982b1192412bf07f5ad185151e6b4536
Mandingueira <jwiecek@onet.pl>
2017-08-31 23:45:56 +0200

    m

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,48 @@
 $(document).ready(function(){
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+  
+  $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+          $(this).addClass("slide");
+        }
+    });
+  });
+    
+var $main = $('#main');
+var $button = $('.btn-custom-lg');
+   
+    $button.on('mouseover', function(e) {
+        $main.attr('height', 0.9 * $main.height());
+        
+    });
+        
+    $button.on('mouseleave', function(e) {
+        $main.attr('height', 1.11 * $main.height()); 
+    });
     
     var $body= $(document.body);
     var navHeight = $('.navbar').outerHeight(true) + 10;
@@ -20,9 +64,12 @@ $(document).ready(function(){
         $('#ok').show()
         
 //        if('.butt').value === null){
-//            alert(Wpisz wartość!)
+//            alert(Wpisz wartoĹ›Ä‡!)
 //        }
                      });
-        
-    
-});
+//    
+//    $('.btn-custom-lg').mouseover(function () {
+//        $('.main').attr('height', 0.9 * ('.main').height());
+//                             });
+
+})
